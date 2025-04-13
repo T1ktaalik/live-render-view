f1f49874cc9c3e03216d010bacde5ff049eb2399
Sergey_Oleynik <tiktaalik@yandex.ru>
2025-04-13 14:29:07 +0300

    reflection-1

--- live-render-1/src/App.tsx
+++ live-render-1/src/App.tsx
@@ -1,27 +1,89 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from "react";
 /* import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg' */
-import './App.css'
-import { Viewer, XKTLoaderPlugin } from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js"
+import "./App.css";
+import {
+  Viewer,
+  XKTLoaderPlugin,
+  Mesh,
+  buildSphereGeometry,
+  ReadableGeometry,
+  MetallicMaterial,
+  ReflectionMap,
+  LightMap,
+  PhongMaterial,
+  Texture,
+  DirLight,
+  PointLight,
+  sRGBEncoding,
+} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js";
 function App() {
- /*  const [count, setCount] = useState(0) */
-  
+  /*  const [count, setCount] = useState(0) */
+
+  let viewer: any = null;
+
+  useEffect(() => {
+    loadViewer();
+  }, []);
+
+  function loadViewer() {
+    viewer = new Viewer({
+      canvasId: "v",
+      transparent: true,
+      colorTextureEnabled: true,
+      pbrEnabled: true,
+    });
+
+    viewer.scene.clearLights();
+
+    new DirLight(viewer.scene, {
+      id: "keyLight",
+      dir: [0.8, -0.6, -0.8],
+      color: [1.0, 0.99, 0.99],
+      intensity: 1.0,
+      space: "view"
+ });
 
 
- const viewer = new Viewer({
-    canvasId: "v"
-  })
+    new ReflectionMap(viewer.scene, {
+      src: [
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_PX.png",
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_NX.png",
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_PY.png",
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_NY.png",
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_PZ.png",
+          "/textures/reflect/Uffizi_Gallery/Uffizi_Gallery_Radiance_NZ.png"
+      ], 
+      encoding: sRGBEncoding
+  });
+   
 
-  const xktLoader = new XKTLoaderPlugin(viewer)
-  const node = xktLoader.load({
-    id: 'theModel', 
-    src: "./assets/glb/DamagedHelmet.textures.glb.xkt"
-  })
+    const xktLoader = new XKTLoaderPlugin(viewer);
+    const node = xktLoader.load({
+      id: "theModel",
+      src: "/glb/DamagedHelmet.textures.glb.xkt",
+    });
+
+    console.log(viewer);
+
+    node.on("loaded", () => {
+      console.log("Model loaded");
+    
+
+      if (viewer) {
+        viewer.cameraFlight.flyTo(node);
+      }
+    });
+  }
+
   return (
     <>
-      <canvas id="v" className="h-screen w-screen bg-sky-300/[.06]"></canvas>
+      <canvas
+        id="v"
+        className="h-screen w-screen bg-sky-300/[.06] absolute overflow-hidden top-0 max-h-screen max-w-screen z-10"
+      ></canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
